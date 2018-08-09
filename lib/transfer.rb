2a6e2529e6fe810d5d7b3ed82f1649370e7e8a9e
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-09 12:09:18 -0400

    lab completed

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,10 +34,19 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-    elsif !(sender.valid?)
-      return "rejected"
-    else
-      "Transaction rejected. Please check your account balance."
+    elsif  !(sender.valid?) == false
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
     end
+  end
+  def reverse_transfer
+    #binding.pry
+  if @status == "complete"
+    sender.balance += amount
+    receiver.balance -= amount
+    @status = 'reversed'
+  end
+  #end
+
   end
 end
