805041933a6acb5688ab8fcf1a7692a33841ac37
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-09 11:34:34 -0400

    pair programming

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :status
+
+  def initialize(person_1,person_2,amount)
+    @person_1 = person_1
+    @person_2 = person_2
+    @amount  = amount
+    @status = 'pending'
+  end
+  def sender
+    @person_1
+  end
+
+  def receiver
+    @person_2
+  end
+  def amount
+    @amount
+  end
+
+  def valid?
+    if sender.status == 'open'  && receiver.status == 'open'
+      true
+    else
+      false
+    end
+  end 
 end
